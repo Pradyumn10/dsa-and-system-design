efea12c63740357a4ac91e188a0759035be24ec0
Pradyumn Joshi <108pradyumn@gmail.com>
2026-01-13 23:24:25 +0530

    jan13-question10-commit

---
+++ dsa/plus_one.py
@@ -0,0 +1,15 @@
+"""
+Date: Jan 13, 2026
+Problem: https://leetcode.com/problems/plus-one
+"""
+from traitlets import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits_str="".join(map(str,digits))
+        digits_int=int(digits_str)+1
+        k=[int(nums) for nums in str(digits_int)]
+        return k
+    
+print(Solution().plusOne([9,9,9]))  # Output: [1,0,0,0]
+print(Solution().plusOne([1,2,3]))  # Output: [1,2,4]
